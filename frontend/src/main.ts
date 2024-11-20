11000d4c5f56fad5daf7ef8e9111ea1fd99ed1f1
Nuki89 <z.spiler46@gmail.com>
2024-11-20 16:58:47 +0100

    feat: installed angular mat/added function for custom criteria

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,10 +11,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule), provideAnimationsAsync()
   ]
 });
