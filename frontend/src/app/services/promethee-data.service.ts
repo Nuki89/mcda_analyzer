6ebe898b68b49a779704154f0f8711ce40e99d9b
Nuki89 <z.spiler46@gmail.com>
2024-12-06 20:28:32 +0100

    feat: added trigger to service for fetching promt data

--- frontend/src/app/services/promethee-data.service.ts
+++ frontend/src/app/services/promethee-data.service.ts
@@ -6,13 +6,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PrometheeDataService {
-
   private apiUrl = 'http://127.0.0.1:8000/promethee-results/';
+  private calculationUrl = 'http://127.0.0.1:8000/promethee/';
 
   constructor(private http: HttpClient) { }
 
   getPrometheeData(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
-}
 
+  triggerPrometheeCalculation(): Observable<any> {
+    return this.http.get(this.calculationUrl); 
+  }
+
+}
