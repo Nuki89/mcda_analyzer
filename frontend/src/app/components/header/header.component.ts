7848f413ed413aec7bdd5b38db250c7cd7897ea7
Nuki89 <z.spiler46@gmail.com>
2024-12-06 19:08:59 +0100

    feat: added scrape button on snowflake when there is no data/or u want to scrape the new one

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DarkModeComponent } from '../dark-mode/dark-mode.component';
 import { RouterModule } from '@angular/router';
 import { faSnowflake } from '@fortawesome/free-solid-svg-icons';
+import { ScrapeService } from '../../services/scrape.service';
 
 @Component({
   selector: 'app-header',
@@ -21,4 +22,26 @@ export class HeaderComponent {
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
+
+  isScraping = false; 
+  scrapeResult: any = null;
+
+  constructor(private scrapeService: ScrapeService) {}
+
+  onScrape(): void {
+    this.isScraping = true;
+    this.scrapeService.scrapeData().subscribe({
+      next: (response) => {
+        console.log('Scrape successful', response); 
+        this.scrapeResult = response; 
+        this.isScraping = false; 
+        window.location.reload();
+      },
+      error: (error) => {
+        console.error('Scrape failed', error);
+        this.isScraping = false; 
+      },
+    });
+  }
+
 }
