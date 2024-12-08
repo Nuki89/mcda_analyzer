d3e028511f1cbb19aa9d2b0e2f93328e40f9ae4d
Nuki89 <z.spiler46@gmail.com>
2024-12-08 22:26:37 +0100

    feat(ahp): updated service/ts/html/css

--- frontend/src/app/services/ahp-data.service.ts
+++ frontend/src/app/services/ahp-data.service.ts
@@ -6,11 +6,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AhpDataService {
-  private apiUrl = 'http://127.0.0.1:8000/ahp/';
+  private apiUrl = 'http://127.0.0.1:8000/ahp-results/';
+  private calculationUrl = 'http://172.0.0.1:8000/ahp/';
 
   constructor(private http: HttpClient) { }
 
   getAHPdata(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
+
+  triggerAHPCalculation(): Observable<any> {
+    return this.http.get(this.calculationUrl); 
+  }
+
 }
