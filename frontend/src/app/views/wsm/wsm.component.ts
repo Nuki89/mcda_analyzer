5bc07689349a0f3893750d98f76cdce4b34c2f20
Nuki89 <z.spiler46@gmail.com>
2024-12-09 02:19:04 +0100

    feat(wsm): added spinner to html/no data

--- frontend/src/app/views/wsm/wsm.component.ts
+++ frontend/src/app/views/wsm/wsm.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-wsm',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, FontAwesomeModule],
   templateUrl: './wsm.component.html',
   styleUrl: './wsm.component.css'
 })
 export class WsmComponent {
+  // Fontawesome icons
+  faSpinner = faSpinner;
+
+  title = "WSM";
+  wsmData = {} = [];
 
 }
