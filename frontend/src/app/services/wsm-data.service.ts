14fa0c162c57e02c78b5f99c15e8c49b08a47cd4
rud0lph89 <z.spiler46@gmail.com>
2024-12-19 21:16:15 +0100

    fix: update for wsm too, forgot

--- frontend/src/app/services/wsm-data.service.ts
+++ frontend/src/app/services/wsm-data.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WsmDataService {
-  private apiUrl = 'http://127.0.0.1:8000/wsm-results/';
+  private apiUrlWsm = environment.apiUrlWsm;
+  // private apiUrl = 'http://127.0.0.1:8000/wsm-results/';
   private calculationUrl = 'http://127.0.0.1:8000/wsm/';
 
   constructor(private http: HttpClient) { }
 
   getWSMdata(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(this.apiUrlWsm);
   }
 
   triggerWSmCalculation(): Observable<any> {
