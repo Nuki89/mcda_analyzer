957e1a749c6d230b15702c304c11fd5915d46e6d
rud0lph89 <z.spiler46@gmail.com>
2024-12-19 21:56:38 +0100

    test backend

--- backend/backend.sh
+++ backend/backend.sh
@@ -8,4 +8,5 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Start the Django development server
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 172.179.236.116:8000
