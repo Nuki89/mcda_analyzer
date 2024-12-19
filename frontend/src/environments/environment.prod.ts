b4333eea2b76b1289de8ea5aa7100fd688f86e3c
rud0lph89 <z.spiler46@gmail.com>
2024-12-19 21:45:33 +0100

    test prod with 0.0.0.0 url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.179.236.116:8000/scraped-data/',
-  apiUrlAHP : 'http://172.179.236.116:8000/ahp-results/',
-  apiUrlPromethee: 'http://172.179.236.116:8000/promethee-results/',
-  apiUrlTopsis: 'http://172.179.236.116:8000/topsis-results/',
-  apiUrlWsm: 'http://172.179.236.116:8000/wsm-results/'
+  // apiUrl: 'http://172.179.236.116:8000/scraped-data/',
+  // apiUrlAHP : 'http://172.179.236.116:8000/ahp-results/',
+  // apiUrlPromethee: 'http://172.179.236.116:8000/promethee-results/',
+  // apiUrlTopsis: 'http://172.179.236.116:8000/topsis-results/',
+  // apiUrlWsm: 'http://172.179.236.116:8000/wsm-results/'
+  apiUrl: 'http://0.0.0.0:8000/scraped-data/',
+  apiUrlAHP: 'http://0.0.0.0:8000/ahp-results/',
+  apiUrlPromethee: 'http://0.0.0.0:8000/promethee-results/',
+  apiUrlTopsis: 'http://0.0.0.0:8000/topsis-results/',
+  apiUrlWsm: 'http://0.0.0.0:8000/wsm-results/'
 };
