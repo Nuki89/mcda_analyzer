48a164d8cc06e172b697287467110276d44a1777
rud0lph89 <z.spiler46@gmail.com>
2024-12-23 20:20:01 +0100

    update: url env

--- frontend/src/app/services/scrape.service.ts
+++ frontend/src/app/services/scrape.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ScrapeService {
-  private scrapeUrl = 'http://172.179.236.116:8000/scrape/';
+  // private scrapeUrl = 'http://172.179.236.116:8000/scrape/';
   // private scrapeUrl = 'http://127.0.0.1:8000/scrape/';
+    private scrapeUrl = environment.scrapeUrl;
 
   constructor(private http: HttpClient) {}
 
