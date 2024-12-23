031e56f043b385ae20b62b661032995bc81cfb51
rud0lph89 <z.spiler46@gmail.com>
2024-12-23 23:35:40 +0100

    fix: changed environments to apiEndpoints in scrape/scrape-data ++ forgot

--- frontend/src/app/services/scrape.service.ts
+++ frontend/src/app/services/scrape.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { apiEndpoints } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +9,7 @@ import { environment } from '../../environments/environment';
 export class ScrapeService {
   // private scrapeUrl = 'http://172.179.236.116:8000/scrape/';
   // private scrapeUrl = 'http://127.0.0.1:8000/scrape/';
-    private scrapeUrl = environment.scrapeUrl;
+    private scrapeUrl = apiEndpoints.scrapeUrl;
 
   constructor(private http: HttpClient) {}
 
